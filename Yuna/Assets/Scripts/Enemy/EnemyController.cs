dbd50fbf1264ec737da89c48c32d67aca5caa4dc
Márcio Tavares <marciotavares@ua.pt>
2025-03-07 18:23:29 +0000

    Created EnemyController (FSM) and base EnemyState
    Created folder to Enemy related Scripts.
    EnemyState is a abstract ScriptableObject with virtual methods.
    EnemyState has Init, Enter, Update, FixedUpdate and Exit methods.
    EnemyController has a current state and a method to change it.
    EnemyController has calls to the four methods of EnemyState.

---
+++ Yuna/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private EnemyState _currentState;
+
+    // States
+    // ...
+
+
+    private void Awake()
+    {
+        InitializeStates();
+    }
+
+    private void Start()
+    {
+        // TransitionToState(...);
+    }
+
+
+    public void TransitionToState(EnemyState state)
+    {
+        if (_currentState != null) _currentState.ExitState(this);
+        _currentState = state;
+        _currentState.EnterState(this);
+    }
+
+
+    private void InitializeStates()
+    {
+        // ...
+    }
+
+    private void Update()
+    {
+        if (_currentState != null) _currentState.UpdateState(this);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_currentState != null) _currentState.FixedUpdateState(this);
+    }
+}
---
+++ Yuna/Assets/Scripts/Enemy/EnemyState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public abstract class EnemyState : ScriptableObject
+{
+    public virtual void InitState(EnemyController player) {}
+
+    public virtual void EnterState(EnemyController player) {}
+
+    public virtual void UpdateState(EnemyController player) {}
+    public virtual void FixedUpdateState(EnemyController player) {}
+
+    public virtual void ExitState(EnemyController player) {}
+}
