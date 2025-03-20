2c0fd406ae931453157792e3c3a4127068349d66
Márcio Tavares <marciotavares@ua.pt>
2025-03-20 19:04:10 +0000

    Create EnemySearchState
    Search State have two phases: Searching and Suspicious.
    In the Search phase, moves to the last sound position.
    If we reach the sound position, the Suspicious phase starts.
    In the Suspicious phase, check for new sounds until the timer ends.
    If the timer ends, transition to PatrolState.
    If the player is detected (during any phase), transition to ChaseState.

---
+++ Yuna/Assets/Scripts/Enemy/States/EnemySearchState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Yuna/Enemy/EnemySearchState")]
+public class EnemySearchState : EnemyState
+{
+    private SearchPhase _phase = SearchPhase.Searching;
+
+    [SerializeField, Tooltip("Time to transition from the suspicious phase to the PatrolState")]
+    private float _suspiciousTime = 5f;
+    private float _timer = 0f;
+
+
+    public override void EnterState(EnemyController enemy) => _phase = SearchPhase.Searching;
+
+    public override void FixedUpdateState(EnemyController enemy)
+    {
+        // Transition to other states
+        if (enemy.PlayerDetection.WasDetected) enemy.TransitionToState(enemy.ChaseState);
+        // ...
+
+        // Move to the last sound position
+        enemy.NavAgent.SetDestination(enemy.SoundDetection.LastSoundPosition.Value);
+
+        // Handle the current phase
+        Action<EnemyController> handlePhase = _phase switch
+        {
+            SearchPhase.Searching => HandleChasing,
+            SearchPhase.Suspicious => HandleSuspicious,
+            _ => throw new NotImplementedException(),
+        };
+        handlePhase.Invoke(enemy);
+    }
+
+
+    private void HandleChasing(EnemyController enemy)
+    {
+        if (enemy.NavAgent.hasPath || enemy.NavAgent.pathPending) return;
+
+        _timer = _suspiciousTime;
+        _phase = SearchPhase.Suspicious;
+        enemy.SoundDetection.CheckedLastPosition();
+    }
+
+    private void HandleSuspicious(EnemyController enemy)
+    {
+        _timer -= Time.fixedDeltaTime;
+
+        if (enemy.SoundDetection.WasSoundDetected) _phase = SearchPhase.Searching;
+        else if (_timer <= 0f) enemy.TransitionToState(enemy.PatrolState);
+    }
+
+
+    public enum SearchPhase
+    {
+        Searching,
+        Suspicious
+    }
+}
