1b878406c533d84d39ef609c845d024daf741b7e
Márcio Tavares <marciotavares@ua.pt>
2025-03-27 20:37:16 +0000

    Created DialogueLine and DialogueSet
    A DialogueLine has speaker and text.
    A DialogueSet is a ScriptableObject with an identifier and list of Lines.

---
+++ Yuna/Assets/Scripts/Dialogue/DialogueLine.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class DialogueLine
+{
+    [field: SerializeField] public string Speaker { get; private set; }
+    [field: SerializeField] public string Text { get; private set; }
+}
---
+++ Yuna/Assets/Scripts/Dialogue/DialogueSet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Yuna/Dialogue/DialogueSet")]
+public class DialogueSet : ScriptableObject
+{
+    [field: SerializeField] public string DialogueId { get; private set; }
+    [SerializeField] private List<DialogueLine> _lines;
+
+    public List<DialogueLine> GetLines() => _lines.GetRange(0, _lines.Count);
+}
