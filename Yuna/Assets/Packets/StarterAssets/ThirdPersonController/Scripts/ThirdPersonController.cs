b40cf601537e021fe74238986b66408a37cdcd96
Rita Guimarães <ritagui@hotmail.com>
2025-04-03 12:42:37 +0100

    Mouse sensitivity fix

--- Yuna/Assets/Packets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Yuna/Assets/Packets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -198,7 +198,7 @@ namespace StarterAssets
         private void CameraRotation()
         {
             // if there is an input and camera position is not fixed
-            if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+            if (_input.look.sqrMagnitude > 0 && !LockCameraPosition)
             {
                 //Don't multiply mouse input by Time.deltaTime;
                 float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
