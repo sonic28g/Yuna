9dec6e52a09424b9ff3cdd92cdaf454d361a7223
Márcio Tavares <marciotavares@ua.pt>
2025-05-14 18:19:56 +0100

    Max/Current Health for enemies
    IsDead is now a calculated property.
    Created ResetHealth method to fill the current health.
    Changed Kill and added Awake.

--- Yuna/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Yuna/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public event Action OnDeath;
-    public bool IsDead { get; private set; } = false;
+    [Header("Health Settings")]
+    [field: SerializeField] public int MaxHealth { get; private set; } = 100;
+    public int CurrentHealth { get; private set; }
 
+    public event Action OnDeath;
+    public bool IsDead => CurrentHealth <= 0;
+
+
+    private void Awake()
+    {
+        ResetHealth();
+    }
+
+
+    public void ResetHealth() => CurrentHealth = MaxHealth;
 
     public void Kill()
     {
-        if (IsDead) return;
-
-        IsDead = true;
+        CurrentHealth = 0;
         OnDeath?.Invoke();
     }
 }
