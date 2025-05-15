0f424a20e19fc4736d8cc45163b29c4178425786
Márcio Tavares <marciotavares@ua.pt>
2025-05-15 16:10:13 +0100

    EnemyDeadState Enter/ExitState logic into methods
    Created StopNavigation and Disable/RestoreComponents methods.

--- Yuna/Assets/Scripts/Enemy/States/EnemyDeadState.cs
+++ Yuna/Assets/Scripts/Enemy/States/EnemyDeadState.cs
@@ -6,11 +6,28 @@ public class EnemyDeadState : EnemyState
     private Behaviour[] _behaviours;
     private bool[] _enableds;
 
+
     public override void EnterState(EnemyController enemy)
+    {
+        StopNavigation(enemy);
+        DisableComponents(enemy);
+    }
+
+    public override void ExitState(EnemyController enemy)
+    {
+        RestoreComponents(enemy);
+    }
+
+
+    private void StopNavigation(EnemyController enemy)
     {
         enemy.NavAgent.isStopped = true;
         enemy.NavAgent.ResetPath();
+    }
 
+
+    private void DisableComponents(EnemyController enemy)
+    {
         // Behaviours to disable
         _behaviours = new Behaviour[] {
             enemy,
@@ -20,16 +37,29 @@ public class EnemyDeadState : EnemyState
         };
 
         // Store the enabled value and disable the behaviours
-        _enableds = new bool[_behaviours.Length];
-        for (int i = 0; i < _behaviours.Length; i++)
+        int behavioursLength = _behaviours.Length;
+        _enableds = new bool[behavioursLength + 1];
+
+        for (int i = 0; i < behavioursLength; i++)
         {
             _enableds[i] = _behaviours[i].enabled;
             _behaviours[i].enabled = false;
         }
+
+        // Disable the collider
+        if (enemy.Collider == null) return;
+        _enableds[behavioursLength] = enemy.Collider.enabled;
+        enemy.Collider.enabled = false;
     }
 
-    public override void ExitState(EnemyController enemy)
+    private void RestoreComponents(EnemyController enemy)
     {
-        for (int i = 0; i < _behaviours.Length; i++) _behaviours[i].enabled = _enableds[i];
+        // Restore the behaviours
+        int behavioursLength = _behaviours.Length;
+        for (int i = 0; i < behavioursLength; i++) _behaviours[i].enabled = _enableds[i];
+
+        // Restore the collider
+        if (enemy.Collider == null) return;
+        enemy.Collider.enabled = _enableds[behavioursLength];
     }
 }
