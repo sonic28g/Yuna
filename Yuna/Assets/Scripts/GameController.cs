89f37b460f4538de6149c77b14a2ff3674754950
Rita Guimarães <ritagui@hotmail.com>
2025-05-25 01:42:46 +0100

    Added keybind feature

--- Yuna/Assets/Scripts/GameController.cs
+++ Yuna/Assets/Scripts/GameController.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 using StarterAssets;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject pauseMenu; 
-    public ThirdPersonController TPController;
 
-    public bool isSettingsShowing = false;
     void Awake()
     {
         //GameSettings settings = GameSettings.LoadFromFile();
@@ -20,32 +18,11 @@ public class GameController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked; // Tranca o cursor ao centro do ecrã
     }
 
-    private void Update() {
-        if(Input.GetKeyDown(KeyCode.Escape) && !isSettingsShowing)
-        {
-            pauseMenu.SetActive(true);
-            PauseGame();
-            Cursor.visible = true; // Esconde o cursor
-            Cursor.lockState = CursorLockMode.None; // Tranca o cursor ao centro do ecrã
-        }
-    }
-
-    public void PauseGame(){
-        Time.timeScale = 0;
-        TPController.LockCameraPosition = true;
-        // parar camera
-        
-    }
-
-    public void ResumeGame(){
-        Time.timeScale = 1;
-        TPController.LockCameraPosition = false;
-        Cursor.visible = false; // Esconde o cursor
-        Cursor.lockState = CursorLockMode.Locked; // Tranca o cursor ao centro do ecrã
-    }
-
-    public void ToggleSettingsShowing(bool option)
+    private void Update() 
     {
-        isSettingsShowing = option;
+
     }
+
+
+
 }
--- Yuna/Assets/Scripts/Menus/MenuController.cs
+++ Yuna/Assets/Scripts/Menus/MenuController.cs
@@ -7,27 +7,60 @@ public class MenuController : MonoBehaviour
     private StarterAssetsInputs starterAssetsInputs;
 
     private bool isOpen;
-    //public GameObject menu;
+    public ThirdPersonController TPController;
+    public StarterAssetsInputs _inputs;
+
+    public bool showingSettings = false; 
+    public bool showingPause = false; 
+    public bool showingKeybinds = false;
+
+    public GameObject pauseMenu;
+    public GameObject settingsMenu;
+    public GameObject keybindsMenu;
+
 
     private void Awake() 
     {
         isOpen = false;
-        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
     }
 
     private void Update() 
     {
-        if (starterAssetsInputs.showMenu)
+        if(_inputs.showMenu)
         {
-            //OpenMenu();
-            starterAssetsInputs.showMenu = false;
-        }
-    }
+            if(!showingPause && !showingSettings && !showingKeybinds)
+            {
+                showingPause = true;
+                pauseMenu.SetActive(true);
+                PauseGame();
+                Cursor.visible = true; // Esconde o cursor
+                Cursor.lockState = CursorLockMode.None; // Tranca o cursor ao centro do ecrã
+            }
+            else if(showingPause && !showingSettings && !showingKeybinds)
+            {
+                ResumeGame();
+                pauseMenu.SetActive(false);
+            }
+            else if(!showingPause && showingSettings && !showingKeybinds)
+            {
+                settingsMenu.SetActive(false);
+                showingSettings = false;
+                showingPause = true;
+                pauseMenu.SetActive(true);
+            }
+            else if(!showingPause && !showingSettings && showingKeybinds)
+            {
+                keybindsMenu.SetActive(false);
+                showingKeybinds = false;
+                showingPause = true;
+                pauseMenu.SetActive(true);
+            }
 
-    public void OpenMenu()
-    {
-        isOpen = !isOpen;
-        //menu.SetActive(isOpen);
+            _inputs.showMenu = false;
+
+        }
+
+        
     }
 
     public void CloseGame()
@@ -40,8 +73,35 @@ public class MenuController : MonoBehaviour
         SceneManager.LoadScene(sceneName);
     }
 
-    public void ToggleMouse()
-    {
+
+    public void PauseGame(){
+        Time.timeScale = 0;
+        TPController.LockCameraPosition = true;
+        // parar camera
         
+    }
+
+    public void ResumeGame(){
+        Time.timeScale = 1;
+        TPController.LockCameraPosition = false;
+        Cursor.visible = false; // Esconde o cursor
+        Cursor.lockState = CursorLockMode.Locked; // Tranca o cursor ao centro do ecrã
+        showingPause = false;
+    }
+
+    public void ToggleSettingsShowing(bool option)
+    {
+        showingSettings = option;
+    }
+
+    
+    public void ToggleKeybindsShowing(bool option)
+    {
+        showingKeybinds = option;
+    }
+
+    public void TogglePauseShowing(bool option)
+    {
+        showingPause = option;
     }
 }
