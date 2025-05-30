83fe2aff99df0ed450e2f00efe156f9c10821975
Márcio Tavares <marciotavares@ua.pt>
2025-05-30 01:55:21 +0100

    Created methods to play open/close/tab sounds

--- Yuna/Assets/Scripts/Journal/DiaryManager.cs
+++ Yuna/Assets/Scripts/Journal/DiaryManager.cs
@@ -10,6 +10,13 @@ public class DiaryManager : MonoBehaviour
 
     private bool noClues = true;
 
+    [Header("Sound Settings")]
+    [SerializeField] private AudioClip _openClip;
+    [SerializeField] private AudioClip _closeClip;
+    [SerializeField] private AudioClip _tabClip;
+    private AudioSource _audioSource;
+
+
     private void Start() {
         diaryText.text = "No clues yet...";
     }
@@ -23,6 +30,8 @@ public class DiaryManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        _audioSource = GetComponent<AudioSource>();
     }
 
     public void UpdateDiary(InspectableData inspectableData)
@@ -35,6 +44,25 @@ public class DiaryManager : MonoBehaviour
 
         diaryText.text += inspectableData.inspectableTitle + ": " + inspectableData.inspectableDescription + "\n";
     
+    }
+
+
+    public void PlayOpenSound()
+    {
+        if (_audioSource == null || _openClip == null) return;
+        _audioSource.PlayOneShot(_openClip);
+    }
+
+    public void PlayCloseSound()
+    {
+        if (_audioSource == null || _closeClip == null) return;
+        _audioSource.PlayOneShot(_closeClip);
+    }
+
+    public void PlayTabSound()
+    {
+        if (_audioSource == null || _tabClip == null) return;
+        _audioSource.PlayOneShot(_tabClip);
     }
 }
 
