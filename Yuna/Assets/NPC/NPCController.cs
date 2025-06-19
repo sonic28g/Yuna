7f1ad60fa11b21f3d4f4169cf60d8504b418cdea
Márcio Tavares <marciotavares@ua.pt>
2025-06-19 23:15:33 +0100

    Created initial NPCController and NPCState
    NPCs will use EnemyHealth, NavAgent, Animator and AudioSource.

---
+++ Yuna/Assets/NPC/NPCController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour
+{
+    private static Action _resetAllNPCsAction;
+    private static Action _saveAllNPCsAction;
+
+    public static void ResetAllNPCs() => _resetAllNPCsAction?.Invoke();
+    public static void SaveAllNPCs() => _saveAllNPCsAction?.Invoke();
+
+    private NPCState _currentState;
+
+    // Shared components
+    public NavMeshAgent NavAgent { get; private set; }
+    public EnemyHealth EnemyHealth { get; private set; }
+    public Animator Animator { get; private set; }
+    public AudioSource AudioSource { get; private set; }
+    // ...
+
+    // States
+    // [field: Header("States")]
+    // ...
+
+    private static readonly string SPEED_ANIMATOR_PARAMETER = "Speed";
+    private static readonly string MOTION_SPEED_ANIMATOR_PARAMETER = "MotionSpeed";
+    private Vector3 _initialPosition;
+    private Quaternion _initialRotation;
+
+    [Header("Sound Settings")]
+    [SerializeField] private AudioClip[] _footstepClips;
+
+
+    private void Awake()
+    {
+        // Search for components
+        NavAgent = GetComponentInChildren<NavMeshAgent>();
+        EnemyHealth = GetComponentInChildren<EnemyHealth>();
+        Animator = GetComponentInChildren<Animator>();
+        AudioSource = GetComponentInChildren<AudioSource>();
+
+        // Store initial position and rotation
+        _initialPosition = transform.position;
+        _initialRotation = transform.rotation;
+
+        // Check for missing components or "invalid states" + initialization
+        if (NavAgent == null) throw new Exception($"NavMeshAgent is missing in {name}");
+        else if (!NavAgent.isOnNavMesh) throw new Exception($"NavMeshAgent is not on the NavMesh in {name}");
+
+        if (EnemyHealth == null) throw new Exception($"EnemyHealth is missing in {name}");
+        else EnemyHealth.OnDeath += OnDeath;
+
+        // Initialize states
+        InitializeStates();
+
+        _resetAllNPCsAction += ResetNPC;
+        _saveAllNPCsAction += SaveNPC;
+    }
+
+
+    // private void Start() => TransitionToState(_);
+    private void OnDeath() {} // => TransitionToState(_);
+    private void OnDestroy() => EnemyHealth.OnDeath -= OnDeath;
+
+
+    public void TransitionToState(NPCState state)
+    {
+        if (_currentState != null) _currentState.ExitState(this);
+        _currentState = state;
+        if (_currentState != null) _currentState.EnterState(this);
+    }
+
+
+    private void InitializeStates()
+    {
+        // ...
+    }
+
+    private T InitializeState<T>(T state) where T : NPCState
+    {
+        if (state == null) throw new Exception($"{typeof(T)} is missing in {name}");
+
+        T newState = Instantiate(state);
+        newState.name = $"{state.name} - {name}";
+        newState.InitState(this);
+        return newState;
+    }
+
+
+    private void Update()
+    {
+        if (_currentState != null) _currentState.UpdateState(this);
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateAnimator();
+        if (_currentState != null) _currentState.FixedUpdateState(this);
+    }
+
+
+    public void ResetNPC()
+    {
+        // Reset position and rotation
+        transform.SetPositionAndRotation(_initialPosition, _initialRotation);
+        if (NavAgent.isOnNavMesh) NavAgent.ResetPath();
+        NavAgent.Warp(_initialPosition);
+
+        // Reset Health and current state
+        EnemyHealth.ResetHealth();
+        // TransitionToState(_);
+    }
+
+    public void SaveNPC()
+    {
+        _initialPosition = transform.position;
+        _initialRotation = transform.rotation;
+    }
+
+
+    private void UpdateAnimator()
+    {
+        if (Animator == null) return;
+
+        Animator.SetFloat(SPEED_ANIMATOR_PARAMETER, NavAgent.velocity.magnitude);
+        Animator.SetFloat(MOTION_SPEED_ANIMATOR_PARAMETER, NavAgent.velocity.magnitude / NavAgent.speed);
+    }
+
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+    private void OnFootstep(AnimationEvent animationEvent)
+    {
+        if (_footstepClips.Length == 0) return;
+        if (animationEvent.animatorClipInfo.weight >= 0.5f) return;
+
+
+        int index = UnityEngine.Random.Range(0, _footstepClips.Length);
+        AudioSource.clip = _footstepClips[index];
+        AudioSource.Play();
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+    private void OnLand(AnimationEvent _) { }
+}
