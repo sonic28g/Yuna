04aaba5b2e7c88979274fa2e60ef42fca37f516c
Márcio Tavares <marciotavares@ua.pt>
2025-06-22 18:01:11 +0100

    Play/Stop methods for BGMPlayer
    Created enum with BGM types.
    Created class to assign types to clips.
    Added audioSource initialization.
    Start by collecting clip candidates and choose a random one to play.

--- Yuna/Assets/Scripts/BGMPlayer.cs
+++ Yuna/Assets/Scripts/BGMPlayer.cs
@@ -1,8 +1,15 @@
+using System.Linq;
 using UnityEngine;
 
 public class BGMPlayer : MonoBehaviour
 {
     public static BGMPlayer Instance { get; private set; }
+
+    private AudioSource _audioSource;
+
+    [Header("BGM Clips")]
+    [SerializeField] private ClipByType[] _bgmClips;
+    private BGMType? _lastType;
 
 
     private void Awake()
@@ -16,23 +23,69 @@ public class BGMPlayer : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        // Initialization
+        bool hasAudioSource = TryGetComponent(out _audioSource);
+        if (!hasAudioSource) throw new System.Exception($"AudioSource component is missing on {name}.");
     }
 
 
-    public void Play()
+    public void Play(BGMType? type = null)
     {
+        // If no type is provided, use the last played type
+        type ??= _lastType;
+
+        // Set the last played type (or do nothing if null)
+        if (type == null) return;
+        _lastType = type;
+
+        // Get candidates for the specified type (and ensure it has a clip)
+        ClipByType[] candidates = _bgmClips
+            .Where(c => c.Type == type && c.Clip != null)
+            .ToArray();
+
+        // Select a random clip from the candidates (or do nothing if none are available)
+        if (candidates.Length == 0) return;
+        ClipByType selected = candidates[Random.Range(0, candidates.Length)];
+        var selectedClip = selected.Clip;
+
+        // If the selected clip is the same as the currently playing one, do nothing
+        if (selectedClip == _audioSource.clip && _audioSource.isPlaying) return;
+
+        // Play the new clip
+        Stop();
+        _audioSource.clip = selectedClip;
+        _audioSource.Play();
     }
 
     public void Stop()
     {
+        if (_audioSource.isPlaying) _audioSource.Stop();
     }
 
 
     public void Muffle()
+
+
+    public enum BGMType
     {
+        // Cutscene + Main Menu
+        MainMenu,
+
+        // Game
+        YunaHouse,
+        Outside,
+        Village,
+        GuardHouse,
+        Werehouse,
+        Market,
+        // ...
     }
 
-    public void Unmuffle()
+    [System.Serializable]
+    public class ClipByType
     {
+        public BGMType Type;
+        public AudioClip Clip;
     }
 }
