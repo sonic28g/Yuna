66de2d915446e02984a8a3afad50ffae48a39253
Márcio Tavares <marciotavares@ua.pt>
2025-06-24 17:35:02 +0100

    Change of BGM according with Area priority

--- Yuna/Assets/Scripts/GameController.cs
+++ Yuna/Assets/Scripts/GameController.cs
@@ -1,14 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private BGMType _bgmType = BGMType.YunaHouse;
+    [SerializeField] private BGMType _defaultBGMType = BGMType.Outside;
+    private readonly List<BGMTypeAreaTrigger> _currentBGMAreas = new();
+    
     public GameObject tutorial;
 
 
-    private void OnEnable()
+    private void Awake() => BGMTypeAreaTrigger.OnBGMTypeAreaChanged += HandleBGMAreaChanged;
+    private void OnDestroy() => BGMTypeAreaTrigger.OnBGMTypeAreaChanged -= HandleBGMAreaChanged;
+
+
+    private void HandleBGMAreaChanged(BGMTypeAreaTrigger trigger, bool entered)
     {
-        if (BGMPlayer.Instance != null) BGMPlayer.Instance.Play(_bgmType);
+        if (trigger == null) return;
+
+        // Add or remove the trigger from the list
+        if (entered && !_currentBGMAreas.Contains(trigger))
+            _currentBGMAreas.Add(trigger);
+        else if (!entered && _currentBGMAreas.Contains(trigger))
+            _currentBGMAreas.Remove(trigger);
+
+        UpdateCurrentArea();
+    }
+
+    private void UpdateCurrentArea()
+    {
+        if (BGMPlayer.Instance == null) return;
+
+        // Order by BGMType priority and select the highest one
+        // If no areas are found, use the default BGM type
+        BGMType bgmType = _currentBGMAreas
+            .Select(a => a.BGMType)
+            .OrderByDescending(a => (int)a)
+            .DefaultIfEmpty(_defaultBGMType)
+            .FirstOrDefault();
+
+        BGMPlayer.Instance.Play(bgmType);
     }
 
 
