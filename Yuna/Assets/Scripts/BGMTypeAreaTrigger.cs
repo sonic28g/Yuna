07ae9852bc239acca9d0ccc6d2041f2b68f2c4e1
Márcio Tavares <marciotavares@ua.pt>
2025-06-24 16:54:58 +0100

    Created BGMTypeAreaTrigger

---
+++ Yuna/Assets/Scripts/BGMTypeAreaTrigger.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System;
+
+[RequireComponent(typeof(BoxCollider))]
+public class BGMTypeAreaTrigger : MonoBehaviour
+{
+    [SerializeField] private bool _showGizmos = true;
+
+    public static event Action<BGMTypeAreaTrigger, bool> OnBGMTypeAreaChanged;
+    [field: SerializeField] public BGMType BGMType { get; private set; } = BGMType.Outside;
+
+
+    private void Awake()
+    {
+        // Ensure the colliders are set to trigger
+        BoxCollider[] colliders = GetComponents<BoxCollider>();
+        foreach (BoxCollider collider in colliders) collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider _)
+    {
+        Debug.LogWarning($"BGMTypeAreaTrigger.OnTriggerEnter: {this}");
+        OnBGMTypeAreaChanged?.Invoke(this, true);
+    }
+
+    private void OnTriggerExit(Collider _)
+    {
+        Debug.LogWarning($"BGMTypeAreaTrigger.OnTriggerExit: {this}");
+        OnBGMTypeAreaChanged?.Invoke(this, false);
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!_showGizmos) return;
+
+        // Select the color based on the area type
+        Gizmos.color = BGMType switch
+        {
+            BGMType.Outside => Color.white,
+            BGMType.YunaHouse => Color.magenta,
+            BGMType.Village => Color.green,
+            BGMType.GuardHouse => Color.blue,
+            BGMType.Werehouse => Color.yellow,
+            BGMType.Market => Color.red,
+            _ => Color.black
+        } * new Color(1, 1, 1, 0.5f);
+
+        // Draw the gizmos for each collider
+        BoxCollider[] colliders = GetComponents<BoxCollider>();
+        foreach (BoxCollider collider in colliders)
+        {
+            Gizmos.matrix = collider.transform.localToWorldMatrix;
+            Gizmos.DrawCube(collider.center, collider.size);
+        }
+        Gizmos.matrix = Matrix4x4.identity;
+    }
+}
