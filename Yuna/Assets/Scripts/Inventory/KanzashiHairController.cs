8485c4c01107913c1d628de21963826bbf8e62df
Rita Guimarães <ritagui@hotmail.com>
2025-06-27 03:02:21 +0100

    Kanzashi now appears on yuna's hair

--- Yuna/Assets/Scripts/Inventory/KanzashiHairController.cs
+++ Yuna/Assets/Scripts/Inventory/KanzashiHairController.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class KanzashiHairController : MonoBehaviour
 {
-    [SerializeField] int maxKanzashi;
-
     private int currentAmmo;
+
+    [SerializeField] GameObject kanzashi1;
+    [SerializeField] GameObject kanzashi2;
+    [SerializeField] GameObject kanzashi3;
+    [SerializeField] GameObject kanzashi4;
+    [SerializeField] GameObject kanzashi5;
 
     void Update()
     {
@@ -13,15 +17,47 @@ public class KanzashiHairController : MonoBehaviour
         switch (currentAmmo)
         {
             case 0:
-                print("0 kanzashi on hair");
+                kanzashi1.SetActive(false);
+                kanzashi2.SetActive(false);
+                kanzashi3.SetActive(false);
+                kanzashi4.SetActive(false);
+                kanzashi5.SetActive(false);
                 break;
             case 1:
-                print("1 kanzashi on hair");
+                kanzashi1.SetActive(true);
+                kanzashi2.SetActive(false);
+                kanzashi3.SetActive(false);
+                kanzashi4.SetActive(false);
+                kanzashi5.SetActive(false);
                 break;
             case 2:
-                print("2 kanzashi on hair");
+                kanzashi1.SetActive(true);
+                kanzashi2.SetActive(true);
+                kanzashi3.SetActive(false);
+                kanzashi4.SetActive(false);
+                kanzashi5.SetActive(false);
                 break;
-
+            case 3:
+                kanzashi1.SetActive(true);
+                kanzashi2.SetActive(true);
+                kanzashi3.SetActive(true);
+                kanzashi4.SetActive(false);
+                kanzashi5.SetActive(false);
+                break;
+            case 4:
+                kanzashi1.SetActive(true);
+                kanzashi2.SetActive(true);
+                kanzashi3.SetActive(true);
+                kanzashi4.SetActive(true);
+                kanzashi5.SetActive(false);
+                break;
+            case 5:
+                kanzashi1.SetActive(true);
+                kanzashi2.SetActive(true);
+                kanzashi3.SetActive(true);
+                kanzashi4.SetActive(true);
+                kanzashi5.SetActive(true);
+                break;
         }
     }
 }
